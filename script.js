40547bb5152b733091743c58664f496d26b47e69
Israel Buscarons <israelbuscarons@gmail.com>
2023-07-16 21:27:03 -0300

    Rename image to imagenes folder

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 function hoverGithub(img) {
-    img.src = "/images/github-logo-hover.png"
+    img.src = "/imagenes/github-logo-hover.png"
 }
 
 function hoverOutGithub(img) {
-    img.src = "/images/github-logo.png"
+    img.src = "/imagenes/github-logo.png"
 }
 
 function hoverTwitter(img) {
-    img.src = "/images/twitter-logo-hover.png"
+    img.src = "/imagenes/twitter-logo-hover.png"
 }
 
 function hoverOutTwitter(img) {
-    img.src = "/images/twitter-logo.png"
+    img.src = "/imagenes/twitter-logo.png"
 }
 
 function hoverLinkedin(img) {
-    img.src = "images/linkedin-logo-hover.png"
+    img.src = "imagenes/linkedin-logo-hover.png"
 }
 
 function hoverOutLinkedin(img) {
-    img.src = "images/linkedin-logo.png"
+    img.src = "imagenes/linkedin-logo.png"
 }
 
 function hoverIB(img) {
-    img.src = "/images/ib-logo-hover.png"
+    img.src = "/imagenes/ib-logo-hover.png"
 }
 
 function hoverOutIB(img) {
-    img.src = "/images/ib-logo.png"
+    img.src = "/imagenes/ib-logo.png"
 }
 function hoverHome(img) {
-    img.src = "/images/home-logo-hover.png"
+    img.src = "/imagenes/home-logo-hover.png"
 }
 
 function hoverOutHome(img) {
-    img.src = "/images/home-logo.png"
+    img.src = "/imagenes/home-logo.png"
 }
