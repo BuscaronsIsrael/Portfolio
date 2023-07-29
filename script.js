967fe2f00fbe9b9ef2752e9e13b0fbea3a8abecf
Israel Buscarons <92610639+BuscaronsIsrael@users.noreply.github.com>
2023-07-29 18:01:46 -0300

    Fix images path

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function hoverGithub(img) {
-    img.src = "/imagenes/github-logo-hover.png"
+    img.src = "imagenes/github-logo-hover.png"
 }
 
 function hoverOutGithub(img) {
-    img.src = "/imagenes/github-logo.png"
+    img.src = "imagenes/github-logo.png"
 }
 
 function hoverTwitter(img) {
-    img.src = "/imagenes/twitter-logo-hover.png"
+    img.src = "imagenes/twitter-logo-hover.png"
 }
 
 function hoverOutTwitter(img) {
-    img.src = "/imagenes/twitter-logo.png"
+    img.src = "imagenes/twitter-logo.png"
 }
 
 function hoverLinkedin(img) {
@@ -23,16 +23,16 @@ function hoverOutLinkedin(img) {
 }
 
 function hoverIB(img) {
-    img.src = "/imagenes/ib-logo-hover.png"
+    img.src = "imagenes/ib-logo-hover.png"
 }
 
 function hoverOutIB(img) {
-    img.src = "/imagenes/ib-logo.png"
+    img.src = "imagenes/ib-logo.png"
 }
 function hoverHome(img) {
-    img.src = "/imagenes/home-logo-hover.png"
+    img.src = "imagenes/home-logo-hover.png"
 }
 
 function hoverOutHome(img) {
-    img.src = "/imagenes/home-logo.png"
+    img.src = "imagenes/home-logo.png"
 }
